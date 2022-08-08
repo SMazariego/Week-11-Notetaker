a914a3448f1fda166d8dcd8b886db01cbaf8155e
SMazariego <102334091+SMazariego@users.noreply.github.com>
2022-08-08 19:11:50 -0400

    initial upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`API server is now located at Port ${PORT}`);
+});
